8301146a2a0ec40af4b6c49e34e40b65db7ede62
Nicolas <nicolas.otten@insa-lyon.fr>
2016-05-23 09:09:50 +0200

    Simplification code serveur pour le live

--- controller/LiveController.js
+++ controller/LiveController.js
@@ -8,48 +8,23 @@ function applyApp(app){
     var self = this;
 
     app.get('/controller/live/cameras', function(req, res){
+        // Return the list of cameras for each site (not each server), but no live images
        
         var cameras = [],
             servers = self.serverManager.servers;
 
-        /**
-          * This piece of code is awful, it has to be rewritten as soon as possible
-          */
         async.each(servers, function(s, cb){
-            // For each server, get the list of cameras
             s.xmlclient.send({
                 "$": {
                     type:"state",
                     id:1
                 }}, function(err, state){
-                    // We now want a live image from each camedra
                     if (!err){
-                        async.each(state.camera, function(c, callback){
-                            c.$.serverId = s.config.id;
-                            var camid = c.$.camid;
-                            s.xmlclient.send({
-                                "$": {
-                                    type:'live',
-                                    mode: 'jpg',
-                                    camid: camid
-                                }}, function(err, img){
-                                    if (!err){
-                                        c.$.image = img.image[0].data;
-                                    }
-                                    callback();
-                                });
-                        }, function(){ 
-                            // At this point, all cameras connected to the server s have been fetched 
-                            // and a live image has been associated to each of them
-                            state.camera.forEach(function(c){
-                                cameras.push(c.$);
-                            });
-                            cb();
-                        });
-                    }
+                        console.log(s);
+                        cb();
+                    } 
                 });
         }, function(){
-            // At this point, all servers have been considered and we can send the data to the client
             res.send(cameras);
         });
     });
