6524ca971903b5cec2e476a07924046c5ac9ed22
Nicolas <nicolas.otten@insa-lyon.fr>
2016-06-10 11:15:54 +0200

    Accordion animation bugged because of col-lg

--- static/accordion.js
+++
@@ -1,6 +0,0 @@
-$('#accordion').on('shown.bs.collapse', function(event){
-    var scroll = event.target.parentElement.offsetTop;
-    $('#accordion').animate({
-        scrollTop: scroll
-    }, 500);
-})
