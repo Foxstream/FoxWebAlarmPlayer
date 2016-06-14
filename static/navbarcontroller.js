f6845e722d85e5050ead03d5c167cfa7004f9961
Nicolas <nicolas.otten@insa-lyon.fr>
2016-06-14 12:56:52 +0200

    Bug fix active links in navbar

--- static/navbarcontroller.js
+++ static/navbarcontroller.js
@@ -1,8 +1,8 @@
 app.controller('navbarcontroller', ["$scope", '$rootScope', '$location', function($scope, $rootScope, $location) {
 
     $scope.isActive = function(href){
-        var url = $location.absUrl().split('/').slice(-1)[0];
-        return href === url;
+        var url = $location.absUrl();
+        return url.indexOf(href) > -1;
     }
 
 }]);
