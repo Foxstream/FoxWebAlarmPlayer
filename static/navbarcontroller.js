d33d994c67f615fcfe448938c88ac85594c69224
Nicolas <nicolas.otten@insa-lyon.fr>
2016-07-12 13:28:06 +0200

    Alarm notification using local storage

--- static/navbarcontroller.js
+++ static/navbarcontroller.js
@@ -2,8 +2,26 @@ app.controller('navbarcontroller', ["$scope", '$rootScope', '$location', 'alarme
 
     $scope.currentpage = $location.absUrl();
 
+    $scope.alarmcounter = localStorage.getItem('alarmcounter') || 0;
+
+    if ($scope.currentpage.indexOf('alarmview') > 0){
+        $scope.alarmcounter = 0;
+        localStorage.setItem('alarmcounter', $scope.alarmcounter);
+    }
+
+
     $scope.isActive = function(href){
         return $scope.currentpage.indexOf(href) > -1;
     };
+
+    var unbind2 = $rootScope.$on("alarm_create", function(data){
+        if ($scope.currentpage.indexOf('alarmview') === 0){
+            $scope.alarmcounter = 0;
+            localStorage.setItem('alarmcounter', $scope.alarmcounter);
+        }
+    });
+
+
+    $scope.$on('$destroy', function () { unbind(); });
 
 }]);
